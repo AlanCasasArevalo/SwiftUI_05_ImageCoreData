b7126a1b47d73b7233bc20566e58f27d251dcf15
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-08-28 20:34:06 +0200

    [FEATURE][LIST IMAGE] * Collection image was implemented

--- SwiftUI_05_ImageCoreData/Models/Place.swift
+++ SwiftUI_05_ImageCoreData/Models/Place.swift
@@ -2,7 +2,7 @@
 import Foundation
 import CoreData
 
-class Place: NSManagedObject {
+class Place: NSManagedObject, Identifiable  {
     @NSManaged public var name: String
     @NSManaged public var imageData: Data
 }
--- SwiftUI_05_ImageCoreData/Views/CollectionView.swift
+++ SwiftUI_05_ImageCoreData/Views/CollectionView.swift
@@ -2,8 +2,33 @@
 import SwiftUI
 
 struct CollectionView: View {
+    
+    @FetchRequest(entity: Place.entity(), sortDescriptors: [
+        NSSortDescriptor(keyPath: \Place.name, ascending: true)
+    ]) var places: FetchedResults<Place>
+    
     var body: some View {
-        Text("Hello, CollectionView!")
+        VStack  {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(self.places) { place in
+                        VStack {
+                            Spacer()
+                            Image(uiImage: UIImage(data: place.imageData) ?? UIImage())
+                                .resizable()
+                                .frame(width: 250, height: 250, alignment: .center)
+                                .cornerRadius(20)
+                                .shadow(color: .black, radius: 5, x: 5, y: 5)
+                                .padding()
+                            Text(place.name)
+                                .foregroundColor(Color(.darkGray))
+                                .font(.system(.largeTitle, design: .rounded))                                
+                            Spacer()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
